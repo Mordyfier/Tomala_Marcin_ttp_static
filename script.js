bbc3d60664db6685a8afbad90adc653ba56057ef
Martin T <mordyyy@outlook.com>
2021-10-31 18:11:46 -0400

    Small fixes

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function getTime () {
     let hours = current.getHours() >= "10" ? current.getHours() : "0"+current.getHours();
     let minutes = current.getMinutes() >= "10" ? current.getMinutes() : "0"+current.getMinutes();
     let seconds = current.getSeconds() >= "10" ? current.getSeconds() : "0"+current.getSeconds();
-    return hours + ":" + minutes + ":" + seconds
+    if (hours > 12) {
+        hours = hours - 12; 
+    }
+    currentTime = hours + ":" + minutes + ":" + seconds;
+    return currentTime;
 }
 
 function updateTime() {
@@ -12,4 +16,3 @@ function updateTime() {
     document.getElementById("time").innerHTML = "The current time is: " + time;
     setTimeout(updateTime, 1000);
 }
-updateTime();
