6a3928195051a1769049ec5d266cf835c9d74de3
Martin T <mordyyy@outlook.com>
2021-10-31 18:25:23 -0400

    fixed 0 check

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 function getTime () {
     let current = new Date();
-    let hours = current.getHours() >= "10" ? current.getHours() : "0"+current.getHours();
-    let minutes = current.getMinutes() >= "10" ? current.getMinutes() : "0"+current.getMinutes();
-    let seconds = current.getSeconds() >= "10" ? current.getSeconds() : "0"+current.getSeconds();
+    let hours = current.getHours();
+    let minutes = current.getMinutes();
+    let seconds = current.getSeconds();
     if (hours > 12) {
         hours = hours - 12; 
     }
-    currentTime = hours + ":" + minutes + ":" + seconds;
+    currentTime = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds);
     return currentTime;
 }
 
 function updateTime() {
     time = getTime()
-    console.log(getTime())
     document.getElementById("time").innerHTML = "The current time is: " + time;
     setTimeout(updateTime, 1000);
+}
+
+function addZero (number) {
+    return number < 10 ? "0"+number : number;
 }
